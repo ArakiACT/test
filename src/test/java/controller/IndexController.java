d75b9e18f7a5b47c9a6fc52eb670a29a2653d283
ArakiACT <shi-araki@ac-tec.co.jp>
2021-08-10 16:31:31 +0900

    second commit

---
+++ src/test/java/controller/IndexController.java
@@ -0,0 +1,24 @@
+package controller;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+	
+	@RequestMapping("/")
+	public String index(Model model) {
+		List<Map<String,Object>> list;
+		list = jdbcTemplate.queryForList("select * from fruits");
+		model.addAttribute("fruits", list);
+		return "index";
+	}
+}
